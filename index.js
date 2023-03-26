3de90c7d70c104ac6600c0c8c9f19d37c8321e98
sakarie <zackhusein894@gmail.com>
2023-03-26 14:15:55 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+// Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function called square
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Arrow function called add
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+  
